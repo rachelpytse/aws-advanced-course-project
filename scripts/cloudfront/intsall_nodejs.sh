8c659d8ae715cb204a3229b0628cefef10991ef8
EC2 Default User <ec2-user@ip-172-31-11-190.us-west-2.compute.internal>
2022-10-22 04:15:16 +0000

    specify node version in intsall_nodejs.sh

--- scripts/cloudfront/intsall_nodejs.sh
+++ scripts/cloudfront/intsall_nodejs.sh
@@ -3,7 +3,7 @@ pwd_initial=$PWD
 cd /home/ec2-user
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 . ~/.nvm/nvm.sh
-nvm install node
+nvm install 16
 source ~/.bash_profile
 source ~/.bashrc
 cd $pwd_initial
